59ece23921ee2f47ad6a10a507553bdb799a6a2c
Cameron Mullins <cmullins510@gmail.com>
2020-05-18 16:33:03 -0700

    updated port call

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const todoListPath = path.join(__dirname + '/todo.json');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
